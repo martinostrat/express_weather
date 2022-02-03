ec44698e947d07484329b43d5c1517cc6ea784bd
Martin <martin.ostrat@khk.ee>
2022-02-03 10:31:07 +0200

    Created form request response with API data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const body = require('body-parser');
 app = express();
 const path = require('path');
 const fetch = require('node-fetch');
@@ -6,6 +7,8 @@ const { response } = require('express');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(body.urlencoded({ extended: false }))
 
 const key = 'b05a100c6e6c27146f81ad75c338bae4';
 let city = 'Tartu';
@@ -25,7 +28,24 @@ app.get('/', (req, res) => {
             temp: temp
         });
     } )
-    
+});
+
+app.post('/', (req, res) => {
+    let city = req.body.cityname;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let description = data.weather[0].description;
+        let city = data.name;
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15);
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        });
+    });
 });
 
 app.listen(3000);
